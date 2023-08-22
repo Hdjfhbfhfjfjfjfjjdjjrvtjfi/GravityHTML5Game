59be780f30a24b7803960384f9ecfd3f5d7a5be4
Hdjfhbfhfjfjfjfjjdjjrvtjfi <kibardin.alecsej@gmail.com>
2023-08-22 23:44:16 +0300

    Player fix

--- Scripts/Game/Player/Player.cs
+++ Scripts/Game/Player/Player.cs
@@ -19,13 +19,24 @@ public class Player : KinematicBody2D
     }
     public override void _PhysicsProcess(float delta)
     {
-        base._PhysicsProcess(delta);
+        base._PhysicsProcess(delta);    
         Vector2 velocity = new Vector2(0, GravityStrenght) * delta * (int)GravityState;
-        KinematicCollision2D collision = MoveAndCollide(velocity);   
+        KinematicCollision2D collision = MoveAndCollide(velocity);
         if (collision != null)
         {
-            State = PlayerState.OnGround;
-            Animations.Animation = "Running";
+            if (State == PlayerState.NotOnGround)
+            {
+                State = PlayerState.OnGround;
+                Animations.Animation = "Running";
+            }
+        }
+        else
+        {
+            if (State == PlayerState.OnGround)
+            {
+                State = PlayerState.NotOnGround;
+                Animations.Animation = "Falling";
+            }
         }
     }
     private void OnGravityChanged()
